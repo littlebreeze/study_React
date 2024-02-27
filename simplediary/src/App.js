72282398fc293428eb7c118bc9e33a0c92ab14a3
이지원 <dlwldnjs950@naver.com>
2024-02-27 13:08:24 +0900

    배열 수정하기

---
+++ simplediary/src/App.js
@@ -0,0 +1,46 @@
+import "./App.css";
+import { useRef, useState } from "react";
+import DiaryEditor from "./DiaryEditer";
+import DiaryList from "./DiaryList";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+
+    setData([newItem, ...data]);
+  };
+
+  const onRemove = (targetId) => {
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    setData(newDiaryList);
+  };
+
+  const onEdit = (targetId, newContent) => {
+    setData(
+      data.map((it) =>
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
+    );
+  };
+
+  return (
+    <div className="App">
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
+    </div>
+  );
+}
+
+export default App;
