60f4956dc333f0a035dd8680fbd0dea8a042c680
littlebird <dlwldnjs950@naver.com>
2024-03-02 20:36:03 +0900

    API 호출 : async, await, fetch, slice, map

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,13 +1,34 @@
 import "./App.css";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import DiaryEditor from "./DiaryEditer";
 import DiaryList from "./DiaryList";
-import Lifecycle from "./Lifecycle";
 
 function App() {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/comments").then((res) =>
+      res.json()
+    );
+
+    const initData = res.slice(0, 20).map((item) => {
+      return {
+        author: item.email,
+        content: item.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -29,16 +50,11 @@ function App() {
   };
 
   const onEdit = (targetId, newContent) => {
-    setData(
-      data.map((it) =>
-        it.id === targetId ? { ...it, content: newContent } : it
-      )
-    );
+    setData(data.map((it) => (it.id === targetId ? { ...it, content: newContent } : it)));
   };
 
   return (
     <div className="App">
-      <Lifecycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
