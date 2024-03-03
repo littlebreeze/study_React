58c97908b95ecadd1ddbbe3d1035e1820b836d74
littlebird <dlwldnjs950@naver.com>
2024-03-03 16:13:24 +0900

    라우팅 : useParams, useSearchParams, useNavigate

--- emotion-diary/src/pages/Edit.js
+++ emotion-diary/src/pages/Edit.js
@@ -1,8 +1,34 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  const mode = searchParams.get("mode");
+
+  console.log(id);
+  console.log(mode);
+
   return (
     <div className="Edit">
       <h1>Edit</h1>
       <p>여기는 일기 편집.</p>
+      <button onClick={() => setSearchParams({ who: "littlebird" })}>QS change</button>
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        HOME으로 가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로가기
+      </button>
     </div>
   );
 };
