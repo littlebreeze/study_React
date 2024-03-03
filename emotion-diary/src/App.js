1726b8caa113087094b242274686719ecfacd1cb
littlebird <dlwldnjs950@naver.com>
2024-03-03 18:14:06 +0900

    프로젝트 기초 : 버튼 컴포넌트, 헤더 컴포넌트 (type에 따른 클래스 적용)

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -5,20 +5,38 @@ import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import Edit from "./pages/Edit";
 import New from "./pages/New";
-import RouteTest from "./components/RouteTest";
+
+// COMPONENTS
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton text={"왼쪽 버튼"} onClick={() => alert("왼쪽 버튼")} type={"positive"} />
+          }
+          rightChild={
+            <MyButton
+              text={"오른쪽 버튼"}
+              onClick={() => alert("오른쪽 버튼누름")}
+              type={"positive"}
+            />
+          }
+        />
         <h2>App.js</h2>
+        <MyButton text={"버튼"} onClick={() => alert("버튼누름")} type={"positive"} />
+        <MyButton text={"버튼"} onClick={() => alert("버튼누름")} type={"negative"} />
+        <MyButton text={"버튼"} onClick={() => alert("버튼누름")} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
           <Route path="/diary/:id" element={<Diary />} />
         </Routes>
-        <RouteTest />
       </div>
     </BrowserRouter>
   );
---
+++ emotion-diary/src/components/MyButton.js
@@ -0,0 +1,13 @@
+const MyButton = ({ text, type, onClick }) => {
+  const btnType = ["positive", "negative", "default"].includes(type) ? type : "default";
+
+  return (
+    <button className={["MyButton", `MyButton_${btnType}`].join(" ")} onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+
+MyButton.defaultProps = { type: "default" };
+
+export default MyButton;
