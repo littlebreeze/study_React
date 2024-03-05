69d0a38d1401ca002f87d181c529fed871396284
이지원 <dlwldnjs950@naver.com>
2024-03-05 11:08:41 +0900

    기초 : 컴포넌트

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,9 +1,14 @@
-import "./App.css";
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
   return (
     <>
-      <h1>안녕 리액트!</h1>
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
---
+++ section05/src/components/Header.jsx
@@ -0,0 +1,10 @@
+// const Header = () => {}
+function Header() {
+  return (
+    <header>
+      <h1>header</h1>
+    </header>
+  );
+}
+
+export default Header;
