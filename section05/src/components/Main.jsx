e9b31620c6694691e1479675a944e7640fa50eb7
이지원 <dlwldnjs950@naver.com>
2024-03-05 16:45:08 +0900

    기초 : 스타일 적용

--- section05/src/components/Main.jsx
+++ section05/src/components/Main.jsx
@@ -1,9 +1,16 @@
+import './Main.css';
+
 const Main = () => {
-  return (
-    <main>
-      <h1>main</h1>
-    </main>
-  );
+  const user = {
+    name: '이지원',
+    isLogin: true,
+  };
+  if (user.isLogin) {
+    return <div className='logout'>로그아웃</div>;
+  } else {
+    return <div>로그인</div>;
+  }
+  //return <>{user.isLogin ? <div>로그아웃</div> : <div>로그인</div>}</>;
 };
 
 export default Main;
