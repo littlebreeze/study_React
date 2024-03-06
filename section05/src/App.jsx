108f2b73dd61aa860b7fa1ded641b6464d6b5aa6
이지원 <dlwldnjs950@naver.com>
2024-03-06 10:59:11 +0900

    기초 : State

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,21 +1,22 @@
 import './App.css';
-import Button from './components/Button';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Main from './components/Main';
+import { useState } from 'react';
 
 function App() {
-  const buttonProps = {
-    text: '메일',
-    color: 'greenyellow',
-  };
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState('OFF');
+
   return (
     <>
-      <Button {...buttonProps} />
-      <Button text={'카페'} />
-      <Button text={'블로그'}>
-        <Header />
-      </Button>
+      <div>
+        <h1>{light}</h1>
+        <button onClick={() => setLight(light === 'OFF' ? 'ON' : 'OFF')}>
+          전구 {light === 'ON' ? '끄기' : '켜기'}
+        </button>
+      </div>
+      <div>
+        <h1>{count}</h1>
+        <button onClick={() => setCount(count + 1)}>+</button>
+      </div>
     </>
   );
 }
