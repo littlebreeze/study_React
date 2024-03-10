970a6963b2e540be96e90a365c8565f041b91bd7
littlebird <dlwldnjs950@naver.com>
2024-03-10 23:32:32 +0900

    파이널프로젝트 : 라우팅

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,10 +1,34 @@
-import { useState } from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import New from "./pages/New";
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import Notfound from "./pages/Notfound";
+
+/*
+  1. / : 모든 일기를 조회하는 페이지 (HOME)
+  2. /new : 새로운 일기를 작성하는 페이지 (NEW)
+  3. /diary : 일기를 상세히 조회하는 페이지 (DIARY)
+  */
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <>app</>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route
+        path="/new"
+        element={<New />}
+      ></Route>
+      <Route
+        path="/diary"
+        element={<Diary />}
+      ></Route>
+      <Route
+        path="*"
+        element={<Notfound />}
+      ></Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ section12/src/pages/Notfound.jsx
@@ -0,0 +1,5 @@
+const Notfound = () => {
+  return <div>NOT FOUND</div>;
+};
+
+export default Notfound;
