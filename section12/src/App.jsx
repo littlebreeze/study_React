4fa7c3af63af9f33052a820bacb7e12f305b422b
이지원 <dlwldnjs950@naver.com>
2024-03-12 21:07:35 +0900

    파이널프로젝트 : Home

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -84,36 +84,13 @@ function App() {
 
   return (
     <>
-      <button
-        onClick={() => {
-          onCreate(new Date().getTime(), 1, 'Hello');
-        }}
-      >
-        일기 추가 테스트
-      </button>
-
-      <button
-        onClick={() => {
-          onUpdate(1, new Date().getTime(), 3, '수정된 일기입니다');
-        }}
-      >
-        일기 수정 테스트
-      </button>
-
-      <button
-        onClick={() => {
-          onDelete(1);
-        }}
-      >
-        일기 삭제 테스트
-      </button>
       <DiaryStateContext.Provider value={data}>
         <DiaryDispatchContext.Provider
           value={{ onCreate, onUpdate, onDelete }}
         >
           <Routes>
-            <Route path='/' element={<Home />}></Route>
             <Route path='/new' element={<New />}></Route>
+            <Route path='/' element={<Home />}></Route>
             <Route path='/diary/:id' element={<Diary />}></Route>
             <Route path='/edit/:id' element={<Edit />}></Route>
             <Route path='*' element={<Notfound />}></Route>
--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -1,9 +1,18 @@
-import { useSearchParams } from "react-router-dom";
+import Header from '../components/Header';
+import Button from '../components/Button';
+import DiaryList from '../components/DiaryList';
 
 const Home = () => {
-  const [params, setParams] = useSearchParams();
-
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header
+        title={'2024년 2월'}
+        leftChild={<Button text={'<'} />}
+        rightChild={<Button text={'>'} />}
+      />
+      <DiaryList />
+    </div>
+  );
 };
 
 export default Home;
