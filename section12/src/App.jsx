50faf1d7bb921ce516bef442da58808a56ab96fd
이지원 <dlwldnjs950@naver.com>
2024-03-12 20:25:13 +0900

    파이널프로젝트 : context 설정

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useReducer, useRef } from 'react';
+import { useReducer, useRef, createContext } from 'react';
 import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 import New from './pages/New';
 import Home from './pages/Home';
@@ -40,6 +40,9 @@ function reducer(state, action) {
       return state;
   }
 }
+
+const DiaryStateContext = createContext();
+const DiaryDispatchContext = createContext();
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
@@ -104,13 +107,19 @@ function App() {
       >
         일기 삭제 테스트
       </button>
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/new' element={<New />}></Route>
-        <Route path='/diary/:id' element={<Diary />}></Route>
-        <Route path='/edit/:id' element={<Edit />}></Route>
-        <Route path='*' element={<Notfound />}></Route>
-      </Routes>
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider
+          value={{ onCreate, onUpdate, onDelete }}
+        >
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/new' element={<New />}></Route>
+            <Route path='/diary/:id' element={<Diary />}></Route>
+            <Route path='/edit/:id' element={<Edit />}></Route>
+            <Route path='*' element={<Notfound />}></Route>
+          </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
     </>
   );
 }
