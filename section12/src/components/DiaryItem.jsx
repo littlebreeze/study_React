f5381e83143999c431164780357ddb280f294028
이지원 <dlwldnjs950@naver.com>
2024-03-13 18:22:41 +0900

    파이널프로젝트 : 상세, 수정 페이지 이동 (useNavigate)

--- section12/src/components/DiaryItem.jsx
+++ section12/src/components/DiaryItem.jsx
@@ -1,20 +1,33 @@
+import { useNavigate } from 'react-router-dom';
+
 import { getEmotionImage } from '../util/get-emotion-image';
 import Button from './Button';
 import './DiaryItem.css';
 
 const DiaryItem = ({ id, emotionId, createdDate, content }) => {
+  const nav = useNavigate();
+
   return (
     <div className='DiaryItem'>
-      <div className={`img_section img_section_${emotionId}`}>
+      <div
+        onClick={() => nav(`/diary/${id}`)}
+        className={`img_section img_section_${emotionId}`}
+      >
         <img src={getEmotionImage(emotionId)} />
       </div>
-      <div className='info_section'>
+      <div
+        onClick={() => nav(`/diary/${id}`)}
+        className='info_section'
+      >
         <div className='created_date'>
           {new Date(createdDate).toLocaleDateString()}
         </div>
         <div className='content'>{content}</div>
       </div>
-      <div className='button_section'>
+      <div
+        onClick={() => nav(`/edit/${id}`)}
+        className='button_section'
+      >
         <Button text={'수정하기'} />
       </div>
     </div>
