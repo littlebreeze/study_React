b22486f4d422763bfdd1ddb72eec0653b0479651
이지원 <dlwldnjs950@naver.com>
2024-04-09 18:01:07 +0900

    typescript

---
+++ udemy-best-react/section30/basic.ts
@@ -0,0 +1,15 @@
+let age: number;
+age = 12;
+
+let userName: string;
+userName = 'Max';
+
+let isInstructor: boolean;
+isInstructor = true;
+
+let hobbies: string[];
+hobbies = ['Sports', 'Cooking'];
+
+let person;
+
+person = { name: 'Max', age: 32 };
---
+++ udemy-best-react/section30/no-typescript.js
@@ -0,0 +1,7 @@
+// function add(a, b) {
+//   return a + b;  // 문자열 붙이는 연산을 진행해버림
+// }
+
+// const result = add('2', '5');  // 처럼 적어주면 문자열로 인식
+
+// console.log(result);
