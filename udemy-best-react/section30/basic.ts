652bdd39b50e7d7893be451565a9f9bd9f21d4cc
이지원 <dlwldnjs950@naver.com>
2024-04-12 16:15:16 +0900

    typescript : 타입 추론, 유니온 타입, type 키워드, 함수, 제네릭

--- udemy-best-react/section30/basic.ts
+++ udemy-best-react/section30/basic.ts
@@ -24,3 +24,38 @@ let people: {
   name: string;
   age: number;
 }[];
+
+let course = 'React';
+// course = 1235; => 오류발생
+
+let course2: string | number = 'React';
+course2 = 1234;
+
+// 위에서 person과 people이 반복
+// type : ts 에만 존재하는 키워드기 때문에 js로 컴파일하면 해당 코드는 사라짐
+type Person = {
+  name: string;
+  age: number;
+};
+
+let person2: Person;
+let people2: Person[];
+
+function add(a: number, b: number) {
+  return a + b; // 반환값에 대한 타입 추론 발생
+}
+
+function print() {
+  console.log('wow');
+}
+
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArr = [1, 2, 3];
+const updatedArr = insertAtBeginning(demoArr, -1);
+const StringArr = insertAtBeginning(['1', '2', '3'], '4');
+updatedArr[0].split('');
+StringArr[0].split('');
