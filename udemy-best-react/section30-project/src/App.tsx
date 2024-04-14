ebd6a50e2ee634f83cfbd72ba85d85948762c557
littlebird <dlwldnjs950@naver.com>
2024-04-14 22:24:57 +0900

    typescript + react : class 정의해서 props 사용하기

--- udemy-best-react/section30-project/src/App.tsx
+++ udemy-best-react/section30-project/src/App.tsx
@@ -1,9 +1,11 @@
 import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
+  const todos = [new Todo("Learn React"), new Todo("Learn Typescript")];
   return (
     <div>
-      <Todos items={["Learn React", "Learn Typescript"]} />
+      <Todos items={todos} />
     </div>
   );
 }
--- udemy-best-react/section30-project/src/components/Todos.tsx
+++ udemy-best-react/section30-project/src/components/Todos.tsx
@@ -1,10 +1,11 @@
 import { FC } from "react";
+import Todo from "../models/todo";
 
-const Todos: FC<{ items: string[] }> = (props) => {
+const Todos: FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item}>{item}</li>
+        <li key={item.id}>{item.text}</li>
       ))}
     </ul>
   );
