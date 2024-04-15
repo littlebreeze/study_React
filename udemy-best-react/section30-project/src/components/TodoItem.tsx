ab52888ec5d402f927f417cbec68be80d20d7eba
이지원 <dlwldnjs950@naver.com>
2024-04-15 18:47:21 +0900

    typescript + react : Todo 제거하기 Max 코드 (bind 함수)

--- udemy-best-react/section30-project/src/components/TodoItem.tsx
+++ udemy-best-react/section30-project/src/components/TodoItem.tsx
@@ -2,12 +2,10 @@ import { FC } from 'react';
 import Todo from '../models/todo';
 import classes from './TodoItem.module.css';
 
-const TodoItem: FC<{ item: Todo; onRemoveTodo: (id: string) => void }> = ({ item, onRemoveTodo }) => {
-  const clickTodoItemHandler = () => {
-    onRemoveTodo(item.id);
-  };
+// onRemoveTodo에 event: React.MouseEvent를 인수로 적어줘도 되지만 여기서 안쓰니까 생략!
+const TodoItem: FC<{ item: Todo; onRemoveTodo: () => void }> = ({ item, onRemoveTodo }) => {
   return (
-    <li className={classes.item} onClick={clickTodoItemHandler}>
+    <li className={classes.item} onClick={onRemoveTodo}>
       {item.id} + {item.text}
     </li>
   );
--- udemy-best-react/section30-project/src/components/Todos.tsx
+++ udemy-best-react/section30-project/src/components/Todos.tsx
@@ -7,7 +7,7 @@ const Todos: FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (props)
   return (
     <ul className={classes.todos}>
       {props.items.map((item) => (
-        <TodoItem key={item.id} item={item} onRemoveTodo={props.onRemoveTodo} />
+        <TodoItem key={item.id} item={item} onRemoveTodo={props.onRemoveTodo.bind(null, item.id)} />
       ))}
     </ul>
   );
