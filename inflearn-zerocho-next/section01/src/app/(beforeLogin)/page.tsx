5319dd08c377c97a6fb0cc67a69dd61e6c1335d0
littlebird <dlwldnjs950@naver.com>
2024-04-20 16:34:29 +0900

    nextjs(zero) : Intercepting Routes (url에 매핑된 page.tsx 가로채기)

--- inflearn-zerocho-next/section01/src/app/(beforeLogin)/page.tsx
+++ inflearn-zerocho-next/section01/src/app/(beforeLogin)/page.tsx
@@ -16,7 +16,7 @@ export default function Main() {
           계정 만들기
         </Link>
         <h3>이미 트위터에 가입하셨나요?</h3>
-        <Link href='/login' className={styles.login}>
+        <Link href='/i/flow/login' className={styles.login}>
           로그인
         </Link>
       </div>
