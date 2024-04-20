713fd2df53aa24c8ca90e4a4b0de381b96781021
littlebird <dlwldnjs950@naver.com>
2024-04-20 17:49:05 +0900

    nextjs(zero) : 공통 컴포넌트 분리

--- inflearn-zerocho-next/section01/src/app/(beforeLogin)/login/page.tsx
+++ inflearn-zerocho-next/section01/src/app/(beforeLogin)/login/page.tsx
@@ -1,30 +1,11 @@
 "use client";
-import styles from "@/app/page.module.css";
-import Image from "next/image";
-import zLogo from "../../../../public/zlogo.png";
-import Link from "next/link";
+
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 import { useRouter } from "next/navigation";
+import Main from "../_component/Main";
 
 export default function Login() {
   const router: AppRouterInstance = useRouter();
   router.replace("/i/flow/login");
-  return (
-    <div className={styles.container}>
-      <div className={styles.left}>
-        <Image src={zLogo} alt='logo' />
-      </div>
-      <div className={styles.right}>
-        <h1>지금 일어나고 있는 일</h1>
-        <h2>지금 가입하세요.</h2>
-        <Link href='/i/flow/signup' className={styles.signup}>
-          계정 만들기
-        </Link>
-        <h3>이미 트위터에 가입하셨나요?</h3>
-        <Link href='/login' className={styles.login}>
-          로그인
-        </Link>
-      </div>
-    </div>
-  );
+  return <Main />;
 }
