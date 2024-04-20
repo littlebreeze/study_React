6668324d9399fdcf2fbe50cb6c106ee7bcccbe17
littlebird <dlwldnjs950@naver.com>
2024-04-20 17:42:13 +0900

    nextjs(zero) : login -> i/flow/login

--- inflearn-zerocho-next/section01/src/app/(beforeLogin)/login/page.tsx
+++ inflearn-zerocho-next/section01/src/app/(beforeLogin)/login/page.tsx
@@ -1,5 +1,30 @@
-import {redirect} from "next/navigation";
+"use client";
+import styles from "@/app/page.module.css";
+import Image from "next/image";
+import zLogo from "../../../../public/zlogo.png";
+import Link from "next/link";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
-  redirect('/i/flow/login')
+  const router: AppRouterInstance = useRouter();
+  router.replace("/i/flow/login");
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <Image src={zLogo} alt='logo' />
+      </div>
+      <div className={styles.right}>
+        <h1>지금 일어나고 있는 일</h1>
+        <h2>지금 가입하세요.</h2>
+        <Link href='/i/flow/signup' className={styles.signup}>
+          계정 만들기
+        </Link>
+        <h3>이미 트위터에 가입하셨나요?</h3>
+        <Link href='/login' className={styles.login}>
+          로그인
+        </Link>
+      </div>
+    </div>
+  );
 }
