7b2071952999260e2063064aca5e0555624040b5
이지원 <dlwldnjs950@naver.com>
2024-04-23 10:24:53 +0900

    next(zero) : 탐색하기 - 검색form, 나를 위한 트랜드 재활용

--- inflearn-zerocho-next/section02/src/app/(afterLogin)/explore/page.tsx
+++ inflearn-zerocho-next/section02/src/app/(afterLogin)/explore/page.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <main>탐색페이지</main>;
+import style from './explore.module.css';
+import SearchForm from '../_component/SearchForm';
+import Trend from '../_component/Trend';
+
+export default function Page() {
+  return (
+    <main className={style.main}>
+      <div className={style.formZone}>
+        <SearchForm />
+      </div>
+      <div className={style.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </main>
+  );
 }
