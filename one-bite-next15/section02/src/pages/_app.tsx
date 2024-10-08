ae3a28536358ab60908fcb481a4abb24d4a4531f
littlebird <dlwldnjs950@naver.com>
2024-10-08 15:32:40 +0900

    next(one-bite): 새 컴포넌트, 루트 컴포넌트, 공통 HTML, strictMode 끔

--- one-bite-next15/section02/src/pages/_app.tsx
+++ one-bite-next15/section02/src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <header>글로벌 헤더</header>
+      <Component {...pageProps} />
+    </>
+  );
 }
