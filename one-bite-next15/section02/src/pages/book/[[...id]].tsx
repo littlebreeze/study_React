180412936b99acb7b54ccaa5ae8a2bea579c699b
littlebird <dlwldnjs950@naver.com>
2024-10-08 16:51:59 +0900

    next(one-bite): 2.2 쿼리스트링, URL Parameter (useRouter), Catsh All Segment [], Optional Catch All Segment [[]]

---
+++ one-bite-next15/section02/src/pages/book/[[...id]].tsx
@@ -0,0 +1,9 @@
+import { useRouter } from 'next/router';
+
+export default function Page() {
+  const router = useRouter();
+  const { id } = router.query;
+  console.log(id);
+
+  return <h1>Book : {id}</h1>;
+}
