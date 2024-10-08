5647232c470b40e30d9ed4d3aa680160ca9f10e9
littlebird <dlwldnjs950@naver.com>
2024-10-09 00:01:01 +0900

    next(one-bite): 2.3 네비게이션 (Link, router 객체 메서드)

--- one-bite-next15/section02/src/pages/_app.tsx
+++ one-bite-next15/section02/src/pages/_app.tsx
@@ -1,10 +1,28 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  // 이벤트 핸들러
+  const onClickButton = () => {
+    router.push('/test');
+  };
+
   return (
     <>
-      <header>글로벌 헤더</header>
+      <header>
+        <Link href={'/'}>index</Link>
+        &nbsp;
+        <Link href={'/search'}>search</Link>
+        &nbsp;
+        <Link href={'/book/1'}>book/1</Link>
+        {/* Programmatic Navigation */}
+        <div>
+          <button onClick={onClickButton}>/test 페이지로 이동</button>
+        </div>
+      </header>
       <Component {...pageProps} />
     </>
   );
